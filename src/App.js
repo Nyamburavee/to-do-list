7d5da9e27fd9a6d6739124535cc92f0c865bb268
[Nyamburavee] <[veronyambura716@gmail.com]>
2024-06-29 12:49:54 +0300

    to do list first committ

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
 function App() {
+  const [todo, setTodo] = useState("")
+    const [todos, setTodos] = useState([])
+    
+    function handleChange(e){
+        setTodo(e.target.value)
+    }
+    
+    function addTodo(){
+        if(todo != ""){
+            setTodos([...todos, todo])
+        console.log(todo) 
+        };
+        setTodo("");
+       
+    };
+    
+    function deleteTodo(text){
+        const newTodos = todos.filter((todo) => {
+            return todo != text
+        })
+        setTodos(newTodos);
+    }
+    
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>To Do List</h1>
+           <div className="">
+           <input
+           name="todo"
+           value={todo}
+           type="text"
+           placeholder="Enter a to do tast"
+           onChange={handleChange}
+           className="todo-input"
+           
+            />
+            <button className="add-button" onClick={addTodo} >Add</button>
+           </div>
+           
+           { todos.length > 0 ? (
+               <ul>
+               {
+                   todos.map((todo, index) => (
+                       <div className="todo-list">
+                       <li key={index}>{todo}</li>
+                       <button className="delete-button" onClick={() =>{
+                           deleteTodo(todo)
+                           }}
+                           >X</button>
+                       </div>
+                   ))
+               }
+               </ul>
+           ):(
+               <div className="no-task">
+               <p>No Task Found</p>
+               </div>
+           )    
+           }  
+      
     </div>
   );
 }
